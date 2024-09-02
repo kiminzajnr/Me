a1f777eaab60a56b4d9eb3a8a9f657467cb3b6a0
kiminzajnr <kiminzaeric@gmail.com>
2024-09-02 11:34:34 +0300

    Add detail page for each project

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 
 app = Flask(__name__)
@@ -30,6 +30,9 @@ projects = [
 ]
 
 
+slug_to_project = {project["slug"]: project for project in projects}
+
+
 @app.route("/")
 def home():
     return render_template("home.html", projects=projects)
@@ -43,3 +46,18 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/project/<string:slug>")
+def project(slug):
+    if slug not in slug_to_project:
+        abort(404)
+    return render_template(
+        f"project_{slug}.html",
+        project=slug_to_project[slug]
+    )
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
